77e15f34d7dc361cd168b1f1603db59bebba7eb4
gidigov <justanoob14@gmail.com>
2018-07-21 05:21:55 +0300

    Initial commit

---
+++ ROR.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ROR
+{
+    class ROR
+    {
+        static active_wars aw = new active_wars(0);
+        static void Main(string[] args)
+        {
+            roll_three_dice();
+            state_money();
+        }
+
+
+        static void roll_three_dice()
+        {
+            Random rnd = new Random();
+            int dice1 = rnd.Next(1, 7);
+            int dice2 = rnd.Next(1, 7);
+            int dice3 = rnd.Next(1, 7);
+            int dice = dice1+dice2+dice3;
+            Console.WriteLine(dice);
+        }
+
+        static int state_money()
+        {
+            int paw = aw.pay_active_wars();
+            int base_state_money = 100;
+            int final_money = base_state_money+paw;
+            Console.WriteLine("state money is: "+final_money);
+            return final_money;
+
+
+        }
+    }
+}
---
+++ fun.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ROR
+{
+    class active_wars
+    {
+        private int activeWars;
+        public active_wars (int active_wars)
+        {
+            this.activeWars = active_wars;
+        }
+        public int get_active_wars()
+        {
+            return this.activeWars;
+        }
+        public void set_active_wars(int active_wars)
+        {
+            this.activeWars = active_wars;
+        }
+        public void inc_active_wars()
+        {
+            this.activeWars++;
+        }
+        public void dec_active_wars()
+        {
+            this.activeWars--;
+        }
+        public int pay_active_wars()
+        {
+            return this.activeWars*-20;
+        }
+    }
+}
